d5faf0a287f6c2ee8bdb46a1d3df1ce216a81aad
eformat <eformat@gmail.com>
2017-03-01 12:54:15 +1000

    java updates

--- src/main/java/org/example/APIRoute.java
+++ src/main/java/org/example/APIRoute.java
@@ -1,10 +1,5 @@
 package org.example;
 
-/**
- * Created by aboucham on 1/19/17.
- */
-
-
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.servlet.CamelHttpTransportServlet;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
@@ -19,9 +14,6 @@ public class APIRoute extends RouteBuilder {
 
     @Bean
     ServletRegistrationBean camelServlet() {
-        // TODO: Camel 2.19 should support this OOTB
-        // use a @Bean to register the Camel servlet which we need to do
-        // because we want to use the camel-servlet component for the Camel REST service
         ServletRegistrationBean mapping = new ServletRegistrationBean();
         mapping.setName("CamelServlet");
         mapping.setLoadOnStartup(1);
